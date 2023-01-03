b752b59ec999e8d63259ffaa4e742ef1dccc25f5
ZahraAhmadi2020 <Ahmadi.work3094@gmail.com>
2023-01-03 21:57:38 +0330

    auto-complete

---
+++ auto complete/component/main.js
@@ -0,0 +1,18 @@
+
+var express = require('express');
+const { append } = require('express/lib/response');
+var host = express();
+var port = 5501;
+
+host.use(express.static(__dirname));
+host.get('/', (request, response) => {
+    response.sendFile(`${__dirname}/component/index.html`);
+});
+
+
+
+host.listen(port, () => {
+    console.log(`app is listening on port ${port}`);
+});
+
+
